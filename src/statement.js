9af02d574a894ec84be5d16856dfa74fdce67021
art <partypeopleland@gmail.com>
2019-04-05 01:58:50 +0800

    提取usd()

--- src/statement.js
+++ src/statement.js
@@ -3,6 +3,17 @@ var createStatementData = require("./data/createStatementData.js");
 function statment(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function usd(aNumber) {
+  return Intl.NumberFormat("eu-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
+}
 
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
@@ -15,19 +26,7 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-  function usd(aNumber) {
-    return Intl.NumberFormat("eu-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
-  }
 }
-
-function htmlStatement(invoice, plays) {
-  return renderHtml(createStatementData(invoice, plays));
-}
-
 function renderHtml(data) {
   let result = `
   <h1>Statement for ${data.customer}</h1>
@@ -44,13 +43,6 @@ function renderHtml(data) {
   <p>You earned <em>${data.totalVolumeCredits}</em> credits</p>
   `;
   return result;
-  function usd(aNumber) {
-    return Intl.NumberFormat("eu-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
-  }
 }
 
 module.exports = {
