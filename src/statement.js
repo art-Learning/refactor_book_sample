3aa5716cf581ec86d71f51e47958a7e754ee6c1b
art <partypeopleland@gmail.com>
2019-04-05 00:49:52 +0800

    以相同步驟移除臨時變數totalAmount

--- src/statement.js
+++ src/statement.js
@@ -44,19 +44,23 @@ module.exports = function statment(invoice, plays) {
     }
     return result;
   }
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
 
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 };
