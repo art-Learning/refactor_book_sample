84c77df8f6a1cf7a8b25e2cf752058da9e90f238
art <partypeopleland@gmail.com>
2019-04-05 00:20:15 +0800

    改變函數聲明，移除play簽章方法

--- src/statement.js
+++ src/statement.js
@@ -3,9 +3,9 @@ module.exports = function statment(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  function amountFor(aPerformance, play) {
+  function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -20,7 +20,7 @@ module.exports = function statment(invoice, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type:${play.type}`);
+        throw new Error(`unknown type:${playFor(aPerformance).type}`);
     }
     return result;
   }
