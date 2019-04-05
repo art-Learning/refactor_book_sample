218532a60c2cf0e208cc4cccebcfff5aa84a866f
art <partypeopleland@gmail.com>
2019-04-05 14:52:51 +0800

    搬移amount()進入計算器

--- src/data/createStatementData.js
+++ src/data/createStatementData.js
@@ -62,4 +62,25 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = play;
   }
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.aPerformance.audience > 30) {
+          result += 1000 * (this.aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.aPerformance.audience > 20) {
+          result += 10000 + 500 * (this.aPerformance.audience - 20);
+        }
+        result += 300 * this.aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type:${this.play.type}`);
+    }
+    return result;
+  }
 }
