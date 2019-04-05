c3665899a4492fd794b11a4cec6a0681a5e33daa
art <partypeopleland@gmail.com>
2019-04-05 14:55:28 +0800

    將原有的函數改寫，讓他調用我們搬移過去的方法

--- src/data/createStatementData.js
+++ src/data/createStatementData.js
@@ -22,25 +22,8 @@ module.exports = function createStatementData(invoice, plays) {
     return plays[aPerformance.playID];
   }
   function amountFor(aPerformance) {
-    let result = 0;
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type:${aPerformance.play.type}`);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .amount;
   }
   function volumeCreditsFor(aPerformance) {
     let result = 0;
@@ -67,16 +50,16 @@ class PerformanceCalculator {
     switch (this.play.type) {
       case "tragedy":
         result = 40000;
-        if (this.aPerformance.audience > 30) {
-          result += 1000 * (this.aPerformance.audience - 30);
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
         }
         break;
       case "comedy":
         result = 30000;
-        if (this.aPerformance.audience > 20) {
-          result += 10000 + 500 * (this.aPerformance.audience - 20);
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
         }
-        result += 300 * this.aPerformance.audience;
+        result += 300 * this.performance.audience;
         break;
       default:
         throw new Error(`unknown type:${this.play.type}`);
