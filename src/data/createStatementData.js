57de90acad7bfd21e89fb25e4b264e7177d0d4fa
art <partypeopleland@gmail.com>
2019-04-05 14:48:50 +0800

    建立演出計算器類別

--- src/data/createStatementData.js
+++ src/data/createStatementData.js
@@ -7,6 +7,8 @@ module.exports = function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance);
+
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
@@ -51,3 +53,9 @@ module.exports = function createStatementData(invoice, plays) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 };
+
+class PerformanceCalculator {
+  constructor(aPerformance) {
+    this.performance = aPerformance;
+  }
+}
